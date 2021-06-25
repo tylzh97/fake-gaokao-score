b4cd2bef06a03c24cb73f8823c4ea7bb0e0769f3
LiZhenghao <tylzh1997@vip.qq.com>
2021-06-25 21:12:37 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,32 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+	info: {
+		name: "刘春江", 
+		id: "4012199709065062",
+		zkz: "00076182",
+		bmh: "202192816293",
+	},
+	score: {
+		yw: 87,
+		sx: 143,
+		yy: 145,
+		zh: 291,
+	}
+  },
+  mounted() {
+  	const url = window.location.href;
+	const b64para = url.match(/b64=([\w\d]+)/g);
+	if (b64para) {
+		const b64 = b64para[0].replace('b64=', '');
+		const b64str = Base64.decode(b64);
+		const data = JSON.parse(b64str);
+		console.log(data);
+		this.info.name = data.name;
+		this.score = data.score;
+	} else {
+		console.log("无数据源");
+	}
+  },
+});
